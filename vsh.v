f389882908f0bbfef5dc22d8cf3951c139600026
Jacob Salmela <me@jacobsalmela.com>
2021-01-18 10:44:38 -0600

    working tui key detection but nothing else
    Signed-off-by: Jacob Salmela <me@jacobsalmela.com>

--- vsh.v
+++ vsh.v
@@ -3,26 +3,57 @@
 
 // use the standard os library
 import os { input, exec, user_os }
+// interpreting what keys were entered
+import term.ui as tui
 
-// A simple prompt
-prompt := 'v# '
+struct Vsh {
+mut:
+	tui           &tui.Context = 0
+}
 
-// Read STDIN (this is the user typing in commands)
-for {
-		stdin := input(prompt)
-
-		// builtin commands this shell can run
-		match stdin {
-			'cd' { println('cd is not yet implemented')
-		         continue }
-			'help' { println('help is not yet implemented')
-			         continue }
-			'exit' { println('Goodbye.')
-			         exit(0) }
-			else { }
+fn event(e &tui.Event, x voidptr) {
+	mut vsh := &Vsh(x)
+	if e.typ == .key_down {
+		match e.code {
+			.escape { exit(0) }
+			.up { println('up') }
+		else {}
 		}
+	}
+}
 
-		stdout := exec(stdin) or { panic(err) }
-		print(stdout.output)
+
+fn main() {
+	mut vsh := &Vsh{
+	}
+
+	vsh.tui = tui.init({
+		event_fn: event
+		capture_events: true
+	})
+	vsh.tui.run()
+
+	// // Read STDIN (this is the user typing in commands)
+	// for {
+	// 		// A simple prompt
+	// 		prompt := 'v# '
+	//
+	// 		mut stdin := os.input(prompt)
+	//
+	// 		// builtin commands this shell can run
+	// 		match stdin {
+	// 			'cd' { println('cd is not yet implemented')
+	// 		         continue }
+	// 			'help' { println('help is not yet implemented')
+	// 			         continue }
+	// 			'exit' { println('Goodbye.')
+	// 			         exit(0) }
+	// 			else { }
+	// 		}
+	//
+	// 		stdout := exec(stdin) or { panic(err) }
+	// 		print(stdout.output)
+	//
+	// }
 
 }
